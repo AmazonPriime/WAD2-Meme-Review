f1bbd5666b2ef396c632807ec3377455a9d64f1e
Selim Hadjadj-Aoul <2396794h@student.gla.ac.uk>
2020-03-16 20:39:10 +0000

    Meme creator ready, some design changes needed

--- meme_review/static/js/meme_creator.js
+++ meme_review/static/js/meme_creator.js
@@ -1,16 +1,15 @@
 //https://www.youtube.com/watch?v=aIgWxXlpRvU
-let canvas,ctx, uploadedImage, topText, bottomText, createButton;
-function createMeme(img, topText, bottomText){
+let canvas,ctx, uploadedImage, topText, bottomText, createButton, fontSize, fontName, outlineWidth, submitButton;
+function createMeme(img, topText, bottomText,fontSize, fontName, outlineWidth){
 	canvas.width = img.width;
 	canvas.height = img.height;
 	ctx.clearRect(0,0,canvas.width, canvas.height);
 	ctx.drawImage(img,0,0);
 	
-	let fontSize = canvas.width/18;
-	ctx.font = fontSize + 'px Arial Black';
+	ctx.font = fontSize + 'px '+fontName;
 	ctx.fillStyle = "white";
 	ctx.strokeStyle = "black";
-	ctx.lineWidth = fontSize / 15;
+	ctx.lineWidth = outlineWidth/10;
 	ctx.textAlign = "center";
 	ctx.textBaseline = "top";
 	topText.split("\n").forEach( function(curText,i){
@@ -31,13 +30,20 @@ function start(){
 	topText = document.getElementById("top");
 	bottomText = document.getElementById("bottom");
 	uploadedImage = document.getElementById("uploadedImage");
+	fontSize = document.getElementById("fontSize");
+	fontName = document.getElementById("fontType");
+	outlineWidth = document.getElementById("outlineWidth");
+	submitButton = document.getElementById("submitButton");
+	submitButton.style.visibility="hidden";
+	var el = document.getElementById("output");
 	createButton = document.getElementById("create-meme");
 	createButton.addEventListener("click", function(){
 		let reader = new FileReader();
 		reader.onload = function(){
 			let img = new Image;
 			img.src = reader.result;
-			createMeme(img,topText.value,bottomText.value);
+			createMeme(img,topText.value,bottomText.value,fontSize.options[fontSize.selectedIndex].value, fontName.options[fontName.selectedIndex].value, outlineWidth.value);
+			submitButton.style.visibility="visible";
 		};
 		reader.readAsDataURL(uploadedImage.files[0]);
 	});

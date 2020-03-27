adb9e0bd33d996c419ef9e924941e499b503183f
AmazonPriime <lukeholland15500@gmail.com>
2020-03-27 19:35:05 +0000

    Fixed issue with empty category causing project to boot

--- meme_review/meme_app/forms.py
+++ meme_review/meme_app/forms.py
@@ -33,7 +33,7 @@ class UserProfileForm(forms.ModelForm):
 
 class MemeForm(forms.ModelForm):
     title = forms.CharField(widget = forms.TextInput(attrs = {'class' : 'form-control', 'placeholder' : 'Title'}))
-    category = forms.CharField(widget = forms.Select(attrs = {'class' : 'form-control'}, choices = [(cat, cat) for cat in Category.objects.all()]))
+    category = forms.ModelMultipleChoiceField(queryset = Category.objects.all(), widget = forms.Select(attrs = {'class' : 'form-control'}))
     nsfw = forms.BooleanField(widget = forms.CheckboxInput(attrs = {'class' : 'form-check-input', 'id' : 'isOver18'}), required=False)
 
     class Meta:

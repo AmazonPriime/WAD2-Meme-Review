87b38479b158994671fbe8bb145d2fff2382ca44
AmazonPriime <lukeholland15500@gmail.com>
2020-03-31 16:25:54 +0100

    Removed allowed hosts

--- meme_review/meme_review/settings.py
+++ meme_review/meme_review/settings.py
@@ -29,7 +29,7 @@ with open(os.path.join(BASE_DIR, 'secret.key')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["memereviews.pythonanywhere.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
